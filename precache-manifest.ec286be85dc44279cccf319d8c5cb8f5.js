8619f184bc1d8742b9d9e9d2b391932d7cca1554
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-29 00:55:31 +0600

    Updates

---
+++ precache-manifest.ec286be85dc44279cccf319d8c5cb8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b111a5fe09883561753eb3b3190d155e",
+    "url": "/Todo-app-react/index.html"
+  },
+  {
+    "revision": "31d96c92619555bb84f2",
+    "url": "/Todo-app-react/static/css/main.fded7107.chunk.css"
+  },
+  {
+    "revision": "29729efa579e214676d3",
+    "url": "/Todo-app-react/static/js/2.00eea824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app-react/static/js/2.00eea824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d96c92619555bb84f2",
+    "url": "/Todo-app-react/static/js/main.caf78c4f.chunk.js"
+  },
+  {
+    "revision": "327a0ab5e43ce1e1ac5c",
+    "url": "/Todo-app-react/static/js/runtime-main.610b2417.js"
+  }
+]);
